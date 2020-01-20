90f12358060380e4ba6bbbec610cc592750d06c3
song.li <song.li@silverera.net>
2020-01-20 13:51:26 +0800

    + support try catch in await.

--- coroutine.h
+++ coroutine.h
@@ -1,177 +1,221 @@
-#include <functional>
-#include <memory>
-#include <list>
 #include <exception>
+#include <functional>
+#include <list>
+#include <memory>
 
-namespace co
-{
-	using namespace std;
+namespace co {
+using namespace std;
 
-	template<typename... A>
-	using Action = function<void(A...)>;
+template <typename... A>
+using Action = function<void(A...)>;
 
-	using ErrorCb = Action<exception_ptr>;
+using ErrorCb = Action<exception_ptr>;
 
-	template<typename T>
-	using Ptr = shared_ptr<T>;
+template <typename T>
+using Ptr = shared_ptr<T>;
 
-
-#define CoBegin(...) \
-    auto __state = 0; \
-    co::Ptr<co::PromiseBase> __promise; \
-    return std::make_shared<co::Promise<__VA_ARGS__>>([=](const co::Action<__VA_ARGS__>& onOk, const co::ErrorCb& onErr) mutable->co::Ptr<co::PromiseBase> { \
+#define CoBegin(...)                  \
+  auto __state = 0;                   \
+  co::Ptr<co::PromiseBase> __promise; \
+    return std::make_shared<co::Promise<__VA_ARGS__>>([=](const co::Action<__VA_ARGS__>& __onOk, const co::ErrorCb& __onErr) mutable->co::Ptr<co::PromiseBase> { \
         try { \
-            switch ( __state ) { case 0: \
+            switch ( __state ) { case 0:
+#define CoAwait(expr)          \
+  do {                         \
+    __state = __LINE__;        \
+    return __promise = expr;   \
+    case __LINE__:             \
+      __promise->checkError(); \
+  } while (0)
 
-#define CoAwaitData(var, ...) \
-                do { __state=__LINE__; return __promise = __VA_ARGS__; case __LINE__: var = std::static_pointer_cast<decltype(__VA_ARGS__)::element_type>(__promise)->getValue(); } while(0)
+#define CoTryAwait(expr, catchBlock) \
+  do {                               \
+    try {                            \
+      __state = __LINE__;            \
+      return __promise = expr;       \
+    } catch catchBlock;              \
+    break;                           \
+    case __LINE__:                   \
+      try {                          \
+        __promise->checkError();     \
+      } catch catchBlock             \
+      ;                              \
+  } while (0)
 
-#define CoAwait(...) \
-                do { __state=__LINE__; return __promise = __VA_ARGS__; case __LINE__: __promise->checkError(); } while(0)
+#define CoAwaitData(var, expr)                                                \
+  do {                                                                        \
+    __state = __LINE__;                                                       \
+    return __promise = expr;                                                  \
+    case __LINE__:                                                            \
+      var = std::static_pointer_cast<decltype(expr)::element_type>(__promise) \
+                ->getValue();                                                 \
+  } while (0)
 
-#define CoReturn(...) \
-                do { onOk(__VA_ARGS__); return nullptr; } while(0) 
+#define CoTryAwaitData(var, expr, catchBlock)                                 \
+  do {                                                                        \
+    try {                                                                     \
+      __state = __LINE__;                                                     \
+      return __promise = expr;                                                \
+    } catch catchBlock;                                                       \
+    break;                                                                    \
+    case __LINE__:                                                            \
+      try {                                                                   \
+        var =                                                                 \
+            std::static_pointer_cast<decltype(expr)::element_type>(__promise) \
+                ->getValue();                                                 \
+      } catch catchBlock                                                      \
+      ;                                                                       \
+  } while (0)
 
-#define CoEnd() \
-            }; \
-        } catch(...) { onErr(std::current_exception()); } return nullptr; \
-    });
+#define CoReturn(...)    \
+  do {                   \
+    __onOk(__VA_ARGS__); \
+    return nullptr;      \
+  } while (0)
 
+#define CoEnd()                        \
+  }                                    \
+  ; /* end switch */                   \
+  } /* end try */                      \
+  catch (...) {                        \
+    __onErr(std::current_exception()); \
+  }                                    \
+  return nullptr;                      \
+  });
 
-	class PromiseBase;
+class PromiseBase;
 
-	class Scheduler
-	{
-	public:
-		static Scheduler*& instance() { static Scheduler* i; return i; }
-		Scheduler() { instance() = this; }
-		virtual ~Scheduler() { instance() = nullptr; }
-		virtual bool updateAll();
-		virtual void add(PromiseBase* i) { pool.push_back(i); }
-		virtual void remove(PromiseBase* i) { pool.remove(i); }
-	private:
-		list<PromiseBase*> pool;
-	};
+class Executor {
+ public:
+  static Executor*& instance() {
+    static Executor* i;
+    return i;
+  }
+  Executor() { instance() = this; }
+  virtual ~Executor() { instance() = nullptr; }
+  virtual bool updateAll();
+  virtual void add(PromiseBase* i) { pool.push_back(i); }
+  virtual void remove(PromiseBase* i) { pool.remove(i); }
 
+ private:
+  list<PromiseBase*> pool;
+};
 
-	class PromiseBase
-	{
-	public:
-		enum class State { Failed, Completed, Inprogres } state = State::Inprogres;
+class PromiseBase {
+ public:
+  enum class State { Failed, Completed, Inprogress } state = State::Inprogress;
 
-		void onError(Action<exception&> cb)
-		{
-			if (state == State::Failed) {
-				try { rethrow_exception(excep); }
-				catch (exception& e) { cb(e); }
-			}
-			else
-				errorCb = cb;
-		}
-		void checkError()
-		{
-			if (state == State::Failed)
-				rethrow_exception(excep);
-		}
-		virtual void update() = 0;
-	protected:
-		PromiseBase() { Scheduler::instance()->add(this); }
-		PromiseBase(const PromiseBase& r) = delete;
-		PromiseBase(PromiseBase&& r) = delete;
-		virtual ~PromiseBase()
-		{
-			auto s = Scheduler::instance();
-			if (s) s->remove(this);
-		}
-		void rejected(exception_ptr e)
-		{
-			state = State::Failed;
-			excep = e;
-			if (errorCb) onError(errorCb);
-		}
-	protected:
-		Ptr<PromiseBase> subFsm;
-		exception_ptr excep;
-		Action<exception&> errorCb;
-	};
+  void onError(Action<exception&> cb) {
+    if (state == State::Failed) {
+      try {
+        rethrow_exception(excep);
+      } catch (exception& e) {
+        cb(e);
+      }
+    } else
+      errorCb = cb;
+  }
+  void checkError() {
+    if (state == State::Failed)
+      rethrow_exception(excep);
+  }
+  virtual void update() = 0;
 
+ protected:
+  PromiseBase() { Executor::instance()->add(this); }
+  PromiseBase(const PromiseBase& r) = delete;
+  PromiseBase(PromiseBase&& r) = delete;
+  virtual ~PromiseBase() {
+    auto s = Executor::instance();
+    if (s)
+      s->remove(this);
+  }
+  void rejected(exception_ptr e) {
+    state = State::Failed;
+    excep = e;
+    if (errorCb)
+      onError(errorCb);
+  }
 
-	template<typename T>
-	class Promise : public PromiseBase
-	{
-	public:
-		template<typename F>
-		Promise(F&& f) : fsm(move(f))
-		{
-			callResolved = [this](T v) { resolved(v); };
-			callError = [this](exception_ptr err) { rejected(err);  };
-			update();
-		}
-		Promise(nullptr_t) {}
-		Promise(T t) { resolved(t); }
+ protected:
+  Ptr<PromiseBase> subFsm;
+  exception_ptr excep;
+  Action<exception&> errorCb;
+};
 
-		void onDone(Action<T> cb)
-		{
-			if (state == State::Completed)
-				cb(value);
-			else
-				okCb = cb;
-		}
-		const T& getValue() { checkError(); return value; }
-	protected:
-		void update() override
-		{
-			if (subFsm && subFsm->state == State::Inprogres) {
-				return;
-			}
-			subFsm = fsm(callResolved, callError);
-		}
-		void resolved(T v)
-		{
-			value = v;
-			state = State::Completed;
-			if (okCb) okCb(value);
-		}
-	private:
-		T value;
-		Action<T> okCb;
-		Action<T> callResolved;
-		ErrorCb callError;
-		function<Ptr<PromiseBase>(const Action<T>& onOK, const ErrorCb& onErr)> fsm;
-	};
+template <typename T>
+class Promise : public PromiseBase {
+ public:
+  template <typename F>
+  Promise(F&& f) : fsm(move(f)) {
+    callResolved = [this](T v) { resolved(v); };
+    callError = [this](exception_ptr err) { rejected(err); };
+    update();
+  }
+  Promise(nullptr_t) {}
+  Promise(T t) { resolved(t); }
 
+  void onDone(Action<T> cb) {
+    if (state == State::Completed)
+      cb(value);
+    else
+      okCb = cb;
+  }
+  const T& getValue() {
+    checkError();
+    return value;
+  }
 
-	//////////////////////////////////////////////////////////////////////////
+ protected:
+  void update() override {
+    if (subFsm && subFsm->state == State::Inprogress) {
+      return;
+    }
+    subFsm = fsm(callResolved, callError);
+  }
+  void resolved(T v) {
+    value = v;
+    state = State::Completed;
+    if (okCb)
+      okCb(value);
+  }
 
-	template<typename T>
-	using PromisePtr = Ptr<Promise<T>>;
+ private:
+  T value;
+  Action<T> okCb;
+  Action<T> callResolved;
+  ErrorCb callError;
+  function<Ptr<PromiseBase>(const Action<T>& onOK, const ErrorCb& onErr)> fsm;
+};
 
-	bool Scheduler::updateAll()
-	{
-		auto inprogress = false;
-		for (auto i : instance()->pool) {
-			if (i->state == PromiseBase::State::Inprogres) {
-				inprogress = true;
-				i->update();
-			}
-		}
-		return inprogress;
-	}
+//////////////////////////////////////////////////////////////////////////
 
-	inline static PromisePtr<bool> all(const list<Ptr<PromiseBase>>& l)
-	{
-		CoBegin(bool)
-		{
-			auto allDone = true;
-			for (auto& i : l) {
-				if (i->state == PromiseBase::State::Inprogres) {
-					allDone = false;
-					break;
-				}
-			}
-			if (allDone) CoReturn(true);
-		}
-		CoEnd()
-	}
+template <typename T>
+using PromisePtr = Ptr<Promise<T>>;
+
+bool Executor::updateAll() {
+  auto inprogress = false;
+  for (auto i : instance()->pool) {
+    if (i->state == PromiseBase::State::Inprogress) {
+      inprogress = true;
+      i->update();
+    }
+  }
+  return inprogress;
 }
 
+inline static PromisePtr<bool> all(const list<Ptr<PromiseBase>>& l) {
+  CoBegin(bool) {
+    auto allDone = true;
+    for (auto& i : l) {
+      if (i->state == PromiseBase::State::Inprogress) {
+        allDone = false;
+        break;
+      }
+    }
+    if (allDone)
+      CoReturn(true);
+  }
+  CoEnd()
+}
+}  // namespace co
--- example.cpp
+++ example.cpp
@@ -1,238 +1,244 @@
 //#include "stdafx.h"
 
+#include <chrono>
 #include <iostream>
 #include <map>
 #include <thread>
-#include <chrono>
 #include "coroutine.h"
-
-using co::PromisePtr;
-using std::map;
-using std::string;
-
-
-enum class ErrorCode
-{
-	OK,
-	Timeout,
-};
-
-struct ExceptionWithCode : std::runtime_error
-{
-	int code;
-	ExceptionWithCode(const char* const msg, int _code)
-		: std::runtime_error(msg), code(_code) {}
-};
-
-template<typename R, typename F, typename... Args>
-PromisePtr<R> promised(F f, Args... args)
-{
-	CoBegin(R)
-	{
-		__state++;
-		f(args..., [=](ErrorCode err, R r) {
-			if (err != ErrorCode::OK) {
-				onErr(std::make_exception_ptr(ExceptionWithCode("promised error", (int)err)));
-			}
-			else {
-				onOk(r);
-			}
-		});
-	}
-	CoEnd()
-}
-
-template<typename F>
-void delay(int ms, F f)
-{
-	std::thread([=] {
-		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-		f();
-	}).detach();
-}
-
-void GetUrl(string url, co::Action<ErrorCode, string> cb)
-{
-	printf("fetching url:%s\n", url.c_str());
-	delay(3000, [=] {
-		cb(ErrorCode::OK, "hello, world. from url:" + url);
-		//cb(ErrorCode::Timeout, {});
-	});
-}
-
-struct AuthData
-{
-	int uid;
-	int sid;
-	string account;
-};
-
-class Handler
-{
-public:
-	PromisePtr<map<string, int>> login(int randCode)
-	{
-		PromisePtr<int> p1, p2;
-
-		CoBegin(map<string, int>)
-		{
-			CoAwait(callInSequence());
-
-			puts("call in parallel");
-			p1 = call("getUID", randCode);
-			p2 = call("getSID", randCode);
-			CoAwait(co::all({ p1, p2 }));
-
-			CoReturn({ {"uid", p1->getValue() },{"sid", p2->getValue() }, });
-		}
-		CoEnd()
-	};
-
-	PromisePtr<int> call(string cmd, int acc)
-	{
-		printf("call:%s\n", cmd.c_str());
-		CoBegin(int)
-		{
-			if (cmd == "getUID")
-				delay(3000, [=] { CoReturn(110); });
-			else if (cmd == "getSID")
-				delay(3000, [=] { CoReturn(220); });
-			else
-				throw std::logic_error("unknown args:" + cmd);
-		}
-		CoEnd()
-	}
-
-	PromisePtr<bool> callInSequence()
-	{
-		puts("call in sequence");
-		string v;
-		CoBegin(bool)
-		{
-			CoAwaitData(v, promised<string>(GetUrl, string("http://test1")));
-			printf("getUrl:%s\n", v.c_str());
-
-			CoAwaitData(v, promised<string>(GetUrl, string("http://test2")));
-			printf("getUrl:%s\n", v.c_str());
-
-			CoReturn(true);
-		}
-		CoEnd()
-	}
-};
-
-
-void testLogin()
-{
-	Handler h;
-
-	static auto prom = h.login(3);
-	prom->onDone([](map<string, int> d) {
-		printf("login done: uid=%d, sid=%d\n", d["uid"], d["sid"]);
-	});
-	prom->onError([](std::exception& e) {
-		printf("login error: %s\n", e.what());
-		if (auto i = dynamic_cast<ExceptionWithCode*>(&e)) {
-			printf("error code:%d", i->code);
-		}
-	});
-}
 
 #include <uv.h>
 
 using namespace co;
 
-template<typename R, typename F, typename Req, typename Cvt, typename... Args>
-PromisePtr<R> uv_promised(F* f, void(*del)(Req*), Cvt rc, Args... args)
-{
-	Req req;
-	CoBegin(R)
-	{
-		__state++;
-		auto cb = [=](Req* req) { rc(req, onOk, onErr); del(req); };
-		using CB = decltype(cb);
-		req.data = new CB(cb);
-		auto ret = f(uv_default_loop(), &req, args..., [](Req* req) { auto cb = (CB*)req->data; (*cb)(req); delete cb; });
-		if (ret < 0)
-			onErr(make_exception_ptr(ExceptionWithCode("promised error", ret)));
-	}
-	CoEnd()
+using co::PromisePtr;
+using std::map;
+using std::string;
+
+enum class ErrorCode {
+  OK,
+  Timeout,
+  InvalidParam,
+};
+
+struct ExceptionWithCode : std::runtime_error {
+  int code;
+  ExceptionWithCode(const char* const msg, int _code)
+      : std::runtime_error(msg), code(_code) {}
+};
+
+template <typename R, typename F, typename... Args>
+PromisePtr<R> promised(F f, Args... args) {
+  CoBegin(R) {
+    __state++;
+    f(args..., [=](ErrorCode err, R r) {
+      if (err != ErrorCode::OK) {
+        __onErr(std::make_exception_ptr(
+            ExceptionWithCode("promised error", (int)err)));
+      } else {
+        __onOk(r);
+      }
+    });
+  }
+  CoEnd()
+}
+
+template <typename F>
+void delay(int ms, F f) {
+  std::thread([=] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    f();
+  })
+      .detach();
+}
+
+void GetUrl(string url, co::Action<ErrorCode, string> cb) {
+  printf("fetching url:%s\n", url.c_str());
+  delay(3000, [=] {
+    if (url.find("http") != 0)
+      return cb(ErrorCode::InvalidParam, {});
+    cb(ErrorCode::OK, "hello, world. from url:" + url);
+    // cb(ErrorCode::Timeout, {});
+  });
+}
+
+struct AuthData {
+  int uid;
+  int sid;
+  string account;
+};
+
+class Handler {
+ public:
+  PromisePtr<map<string, int>> login(int randCode) {
+    PromisePtr<int> p1, p2;
+
+    CoBegin(map<string, int>) {
+      CoAwait(callInSequence());
+
+      puts("call in parallel");
+      p1 = call("getUID", randCode);
+      p2 = call("getSID", randCode);
+      CoAwait(co::all({p1, p2}));
+
+      CoReturn({
+          {"uid", p1->getValue()},
+          {"sid", p2->getValue()},
+      });
+    }
+    CoEnd()
+  };
+
+  PromisePtr<int> call(string cmd, int acc) {
+    printf("call:%s\n", cmd.c_str());
+    CoBegin(int) {
+      if (cmd == "getUID")
+        delay(3000, [=] { CoReturn(110); });
+      else if (cmd == "getSID")
+        delay(3000, [=] { CoReturn(220); });
+      else
+        throw std::logic_error("unknown args:" + cmd);
+    }
+    CoEnd()
+  }
+
+  PromisePtr<bool> callInSequence() {
+    puts("call in sequence");
+    string v;
+    CoBegin(bool) {
+      CoTryAwaitData(v, promised<string>(GetUrl, string("ttp://test2")),
+                     (ExceptionWithCode & e) {
+                       v = "";
+                       printf("exception: %s, code: %d\n", e.what(), e.code);
+                     });
+      if (v.length())
+        printf("getUrl:%s\n", v.c_str());
+
+      CoAwaitData(v, promised<string>(GetUrl, string("http://test1")));
+      printf("getUrl:%s\n", v.c_str());
+
+      CoReturn(true);
+    }
+    CoEnd()
+  }
+};
+
+void testLogin() {
+  Handler h;
+
+  static auto prom = h.login(3);
+  prom->onDone([](map<string, int> d) {
+    printf("login done: uid=%d, sid=%d\n", d["uid"], d["sid"]);
+  });
+  prom->onError([](std::exception& e) {
+    printf("login error: %s\n", e.what());
+    if (auto i = dynamic_cast<ExceptionWithCode*>(&e)) {
+      printf("error code:%d", i->code);
+    }
+  });
+}
+
+template <typename R, typename F, typename Req, typename Cvt, typename... Args>
+PromisePtr<R> uv_promised(F* f, void (*del)(Req*), Cvt rc, Args... args) {
+  Req req;
+  CoBegin(R) {
+    __state++;
+    auto cb = [=](Req* req) {
+      rc(req, __onOk, __onErr);
+      del(req);
+    };
+    using CB = decltype(cb);
+    req.data = new CB(cb);
+    auto ret = f(uv_default_loop(), &req, args..., [](Req* req) {
+      auto cb = (CB*)req->data;
+      (*cb)(req);
+      delete cb;
+    });
+    if (ret < 0)
+      __onErr(make_exception_ptr(ExceptionWithCode("promised error", ret)));
+  }
+  CoEnd()
 }
 
 PromisePtr<int> fs_open(const char* fname, int mode) {
-	return uv_promised<int>(uv_fs_open, uv_fs_req_cleanup, [](uv_fs_t* req, Action<int> ok, ErrorCb err) {
-		if (req->result < 0) return err(make_exception_ptr(ExceptionWithCode("fs_open error", (int)req->result)));
-		ok((int)req->result);
-	}, fname, mode, 0);
+  return uv_promised<int>(uv_fs_open, uv_fs_req_cleanup,
+                          [](uv_fs_t* req, Action<int> ok, ErrorCb err) {
+                            if (req->result < 0)
+                              return err(make_exception_ptr(ExceptionWithCode(
+                                  "fs_open error", (int)req->result)));
+                            ok((int)req->result);
+                          },
+                          fname, mode, 0);
 }
 
 PromisePtr<bool> fs_close(uv_file fd) {
-	return uv_promised<bool>(uv_fs_close, uv_fs_req_cleanup, [](uv_fs_t* req, Action<int> ok, ErrorCb err) {
-		ok(true);
-	}, fd);
+  return uv_promised<bool>(
+      uv_fs_close, uv_fs_req_cleanup,
+      [](uv_fs_t* req, Action<int> ok, ErrorCb err) { ok(true); }, fd);
 };
 
 PromisePtr<const char*> fs_read(uv_file fd, int offset) {
-	static char buffer[255];
-	static auto iov = uv_buf_init(buffer, sizeof(buffer) - 1);
-	return uv_promised<const char*>(uv_fs_read, uv_fs_req_cleanup, [=](uv_fs_t* req, Action<const char*> ok, ErrorCb err) {
-		if (req->result < 0) return err(make_exception_ptr(ExceptionWithCode("fs_read error", (int)req->result)));
-		buffer[req->result] = 0;
-		if (req->result == 0) return ok(nullptr);
-		ok(buffer);
-	}, fd, &iov, 1, offset);
+  static char buffer[255];
+  static auto iov = uv_buf_init(buffer, sizeof(buffer) - 1);
+  return uv_promised<const char*>(
+      uv_fs_read, uv_fs_req_cleanup,
+      [=](uv_fs_t* req, Action<const char*> ok, ErrorCb err) {
+        if (req->result < 0)
+          return err(make_exception_ptr(
+              ExceptionWithCode("fs_read error", (int)req->result)));
+        buffer[req->result] = 0;
+        if (req->result == 0)
+          return ok(nullptr);
+        ok(buffer);
+      },
+      fd, &iov, 1, offset);
 }
 
-PromisePtr<string> fs_read_all(const char* fname)
-{
-	string content;
-	const char* cur;
-	int fd;
-	CoBegin(string)
-	{
-		CoAwaitData(fd, fs_open(fname, O_RDONLY));
-		for (;;) {
-			CoAwaitData(cur, fs_read(fd, content.length()));
-			if (cur == nullptr)
-				break;
-			content += cur;
-		}
-		CoAwait(fs_close(fd));
-		CoReturn(content);
-	}
-	CoEnd();
+PromisePtr<string> fs_read_all(const char* fname) {
+  string content;
+  const char* cur;
+  int fd;
+  CoBegin(string) {
+    CoAwaitData(fd, fs_open(fname, O_RDONLY));
+    for (;;) {
+      CoAwaitData(cur, fs_read(fd, content.length()));
+      if (cur == nullptr)
+        break;
+      content += cur;
+    }
+    CoAwait(fs_close(fd));
+    CoReturn(content);
+  }
+  CoEnd();
 };
 
 list<PromisePtr<string>> tasks;
-void testLibuv()
-{
-	auto p = fs_read_all(__FILE__);
-	p->onDone([](string s) {
-		printf("done: sz=%d\n", s.length());
-	});
-	p->onError([](std::exception& e) {
-		printf("libuv error: %s\n", e.what());
-		if (auto i = dynamic_cast<ExceptionWithCode*>(&e)) {
-			printf("error code:%d", i->code);
-		}
-	});
-	tasks.push_back(p);
+void testLibuv() {
+  auto p = fs_read_all(__FILE__);
+  p->onDone([](string s) { printf("done: sz=%d\n", s.length()); });
+  p->onError([](std::exception& e) {
+    printf("libuv error: %s\n", e.what());
+    if (auto i = dynamic_cast<ExceptionWithCode*>(&e)) {
+      printf("error code:%d", i->code);
+    }
+  });
+  tasks.push_back(p);
 }
 
-int main()
-{
-	Scheduler sc;
-	for (auto i = 0; i < 500; i++)
-		testLibuv();
-	//testLogin();
+int main() {
+  Executor sc;
 
-	int cnt = 0;
-	for (auto run = true; run;) {
-		run = sc.updateAll();
+  testLogin();
 
-		printf("\r%c", "-\\|-/"[cnt++ % 5]);
-		std::this_thread::sleep_for(chrono::milliseconds(30));
+  for (auto i = 0; i < 500; i++)
+    testLibuv();
 
-		uv_run(uv_default_loop(), UV_RUN_ONCE);
-	}
+  int cnt = 0;
+  for (auto run = true; run;) {
+    run = sc.updateAll();
+
+    printf("\r%c", "-\\|-/"[cnt++ % 5]);
+    std::this_thread::sleep_for(chrono::milliseconds(30));
+
+    uv_run(uv_default_loop(), UV_RUN_ONCE);
+  }
 }
